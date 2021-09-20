c1650b04e6aaf742205fa076be8db399a439a51a
Gabriel Guimaraes <gabriel@brex.com>
2021-09-19 21:42:24 -0300

    Basic claimToken works

--- contract/contracts/EventTokens.sol
+++ contract/contracts/EventTokens.sol
@@ -3,23 +3,37 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 
 
 contract EventTokens is ERC1155, Ownable {
-  // TODO: Everything
-  // The below "GameItems' are from OpenZeppelin's example (https://docs.openzeppelin.com/contracts/3.x/erc1155)
-  // We should override
-  uint256 public constant GOLD = 0;
-  uint256 public constant SILVER = 1;
-  uint256 public constant THORS_HAMMER = 2;
-  uint256 public constant SWORD = 3;
-  uint256 public constant SHIELD = 4;
+  address private _internalPublicKey;
 
-  constructor() ERC1155("https://game.example/api/item/{id}.json") {
-    _mint(msg.sender, GOLD, 10**18, "");
-    _mint(msg.sender, SILVER, 10**27, "");
-    _mint(msg.sender, THORS_HAMMER, 1, "");
-    _mint(msg.sender, SWORD, 10**9, "");
-    _mint(msg.sender, SHIELD, 10**9, "");
+  constructor(address publicKey) ERC1155("https://game.example/api/item/{id}.json") {
+    _internalPublicKey = publicKey;
+  }
+
+  function claimToken(uint256 tokenId, uint256 n, bytes memory signature) public virtual {
+    // Require that the signer of _hash(tokenId, n) was the private key corresponding to _internalPublicKey
+    require(
+      SignatureChecker.isValidSignatureNow(_internalPublicKey, hash(tokenId, n), signature),
+      "Wrong signature"
+    );
+
+    _mint(msg.sender, tokenId, 1, "");
+  }
+
+  function hash(uint256 tokenId, uint256 n) public pure returns (bytes32) {
+    bytes32 hashedMsg = keccak256(abi.encode(tokenId, n));
+    return keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hashedMsg));
+  }
+
+  function testRecover(uint256 tokenId, uint256 n, bytes memory signature) public pure returns (address) {
+    return ECDSA.recover(hash(tokenId, n), signature);
+  }
+
+  function rawRecover(bytes32 myHash, bytes memory signature) public pure returns (address) {
+    return ECDSA.recover(myHash, signature);
   }
 }
--- contract/test/event-tokens.js
+++ contract/test/event-tokens.js
@@ -1,8 +1,14 @@
 const assert = require('assert');
+const { expect } = require("chai");
 const { time } = require("@openzeppelin/test-helpers");
 const { ethers } = require("hardhat");
-const { expect } = require("chai");
 
+
+const hash = (tokenId, n) => {
+  return ethers.utils.arrayify(ethers.utils.keccak256(ethers.utils.defaultAbiCoder.encode(["uint256", "uint256"], [tokenId, n])));
+}
+
+const SIGNER = 0;
 
 describe("EventTokens", () => {
   let et, accounts, signers;
@@ -10,15 +16,35 @@ describe("EventTokens", () => {
   before(async () => {
     // Deploy contracts
     const etFactory = await ethers.getContractFactory('EventTokens');
-    et = await etFactory.deploy();
-    assert.notEqual(et, undefined, "EventTokens contract instance is undefined.");
 
     signers = await ethers.getSigners();
     accounts = signers.map(s => s.address);
 
+    et = await etFactory.deploy(accounts[SIGNER]);
+    assert.notEqual(et, undefined, "EventTokens contract instance is undefined.");
   });
 
-  it("TODO", async () => {
-    assert.notEqual(et.address, undefined, "Contract address must not be undefined.");
+  it("Hash works", async () => {
+    const [tokenId, n] = [1, 2];
+    expect(await et.hash(tokenId, n)).to.equal(ethers.utils.hashMessage(hash(tokenId, n)));
+  });
+
+  it("Cannot claim token with wrong signature", async () => {
+    const [tokenId, n] = [1, 2];
+    const badSignature = signers[2].signMessage(hash(tokenId, n));
+    await expect(et.claimToken(tokenId, n, badSignature)).to.be.revertedWith('Wrong signature');
+  });
+
+  it("Claims token with correct signature", async () => {
+    const [tokenId, n] = [1, 2];
+    const correctSignature = signers[SIGNER].signMessage(hash(tokenId, n));
+
+    expect(await et.balanceOf(accounts[3], tokenId)).to.equal(0);
+
+    await et.connect(signers[3]).claimToken(tokenId, n, correctSignature);
+
+    expect(await et.balanceOf(accounts[3], tokenId)).to.equal(1);
+
+
   });
 });
