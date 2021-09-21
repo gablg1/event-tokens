6b470964478e52896c514be097eb8e7de9ac132a
Gabriel Guimaraes <gabriel@brex.com>
2021-09-21 08:54:47 -0300

    Import bootstrap always

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,6 +10,7 @@ import { CreateEvent } from './pages/CreateEvent'
 import { All } from './pages/All'
 import { ViewNFT } from './pages/ViewNFT'
 import { NotificationsList } from './components/Transactions/History'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 export function App() {
   return (
